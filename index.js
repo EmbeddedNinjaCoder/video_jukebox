92675aee60b977b8b4f36e6afed06cf3a32ff893
Monica Gerard <mlgvla@gmail.com>
2023-04-11 23:06:01 -0400

    solution

--- index.js
+++ index.js
@@ -1,67 +1,54 @@
-const form = document.querySelector("#playlistForm")
+const form = document.querySelector("#playlist-form")
 const playlist = document.querySelector("#playlist")
 const player = document.querySelector("#player")
-
-// const inputs = document.querySelectorAll("input")
-
-// inputs.forEach((input) => {
-//    input.addEventListener("focus", (e) => {
-//       e.target.style.backgroundColor = "yellow"
-//    })
-//    input.addEventListener("blur", (e) => {
-//       e.target.style.backgroundColor = ""
-//    })
-// })
 
 let songs = []
 
 form.addEventListener("submit", function (e) {
    e.preventDefault()
 
-   const songNameInput = document.querySelector("#songName")
-   const artistNameInput = document.querySelector("#artistName")
-   const youtubeLinkInput = document.querySelector("#youtubeLink")
-
+   const songNameInput = document.querySelector("#song")
+   const artistNameInput = document.querySelector("#artist")
+   const youtubeIDInput = document.querySelector("#youtubeID")
    const songName = songNameInput.value
    const artistName = artistNameInput.value
-   const youtubeLink = youtubeLinkInput.value
+   const youtubeID = youtubeIDInput.value
 
-   
+   const songObj = {
+      songName,
+      artistName,
+      youtubeID,
+   }
 
-   songs.push(song)
-
-   displayPlaylist()
-
-   //    songNameInput.value = ""
-   //    artistNameInput.value = ""
-   //    youtubeLinkInput.value = ""
+   displayPlaylist(songObj)
 
    form.reset()
 })
 
-function displayPlaylist() {
-   playlist.innerHTML = ""
+function displayPlaylist(songObj) {
+   const span = document.createElement("span")
+   const li = document.createElement("li")
+   const deleteBtn = document.createElement("button")
 
-   songs.forEach(function (song) {
-      const span = document.createElement("span")
-      span.textContent = `${song.songName} - ${song.artistName}`
-      const li = document.createElement("li")
-      const deleteBtn = document.createElement("button")
-      deleteBtn.innerHTML = "🗑️"
-      deleteBtn.addEventListener("click", (e) => {
-         e.stopPropagation()
-         if (confirm("Are you sure you want to remove this song?"))
-            e.target.parentNode.parentNode.remove()
-      })
+   span.textContent = `${songObj.songName} - ${songObj.artistName}   `
+   deleteBtn.innerHTML = "🗑️"
 
-      li.addEventListener("click", function () {
-         player.src = `https://www.youtube.com/embed/${song.youtubeLink}`
-      })
-      span.appendChild(deleteBtn)
-      li.appendChild(span)
-      playlist.appendChild(li)
+   // add event listeners
+
+   li.addEventListener("click", () => {
+      player.src = `https://www.youtube.com/embed/${songObj.youtubeID}`
    })
+
+   deleteBtn.addEventListener("click", (e) => {
+      e.stopPropagation()
+      console.log(e)
+      if (confirm("Are you sure you want to remove this song?"))
+         e.target.parentNode.parentNode.remove()
+   })
+
+   // attach
+   span.appendChild(deleteBtn)
+   li.appendChild(span)
+
+   playlist.appendChild(li)
 }
-// 🗑️
-// make a delete button
-// make a favorite button
